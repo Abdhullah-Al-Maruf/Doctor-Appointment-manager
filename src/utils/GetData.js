fd811728c517fc5066f67d0b2c2651fd2d728e21
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-05-22 14:26:17 +0600

    secured the all booking page with jwt

--- src/utils/GetData.js
+++ src/utils/GetData.js
@@ -8,7 +8,17 @@ export const getDoctorsData = async () => {
 };
 
 export const getBookingsData = async () => {
-  const res = await fetch("http://localhost:5000/appointments");
+
+    // jwt token  for server side authentication
+  const { token } = await auth.api.getToken({
+    headers: await headers(),
+  });
+console.log(token);
+  const res = await fetch("http://localhost:5000/appointments", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
   const data = await res.json();
   return data;
 };
