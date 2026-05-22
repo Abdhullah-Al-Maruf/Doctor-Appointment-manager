c836e516ca702f4aa3ba248ec4a4c19355f47e90
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-05-22 10:45:13 +0600

    configue auth-client and auth.js for jwt token genration

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,13 @@
+import { jwtClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
     baseURL: "http://localhost:3000"
+ 
+    // these steps are for jwt token generation and verification, you can customize it as per your requirements
+    // step-3
+   , plugins: [
+    jwtClient()
+]
+// now the token will be automatically included in the Authorization header for all requests made using authClient
 })
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { jwt } from "better-auth/plugins";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 // await client.connect()
@@ -8,17 +9,28 @@ const db = client.db("skillSphere-online-platform");
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
-    client
+    client,
   }),
-   emailAndPassword: {
-    	enabled: true
-    
+  emailAndPassword: {
+    enabled: true,
   },
-    baseURL: process.env.BETTER_AUTH_URL, 
-    socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID , 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        }, 
+  baseURL: process.env.BETTER_AUTH_URL,
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
+  },
+  
+  // this steps are for jwt token generation and verification, you can customize it as per your requirements
+  // step 2
+  session: {
+    cookieCache: {
+      enabled: true,
+      strategy: "jwt",
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    },
+  },
+  // step-1
+  plugins: [jwt()],
 });
